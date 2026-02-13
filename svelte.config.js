60070fbdf54b04dba99ba81fce1ac4064965299f
Shribalaji <jilabaji@gmail.com>
2026-02-13 13:25:19 +0530

    build issue fix 4 - dynamic routes handled in build

--- svelte.config.js
+++ svelte.config.js
@@ -1,26 +1,11 @@
-import adapter from '@sveltejs/adapter-static';
-
-const dev = process.argv.includes('dev');
+import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
+			out: 'build'
 		}),
-		paths: {
-			// GitHub Pages serves at https://<user>.github.io/<repo>/
-			base: dev ? '' : '/sona-Svelte'
-		},
-		prerender: {
-			entries: ['/', '/dashboard', '/enterprises', '/login', '/robots', '/users'],
-			handleHttpError: 'warn',
-			handleUnseenRoutes: 'warn'
-		},
 		appDir: 'app'
 	}
 };
